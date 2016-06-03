d6891f229615e7609bf6c163ab8396566a5e61d7
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-06-03 17:57:12 +0200

    Add function to dump flash from CM shell

--- cm.c
+++ cm.c
@@ -1,5 +1,34 @@
 #include "bcm2dump.h"
 
+bool cm_flash_read(int fd, const char *part, unsigned addr, void *buf, size_t len)
+{
+	if (len % 16) {
+		fprintf(stderr, "error: length must be multiple of 16\n");
+		return false;
+	}
+
+	bool ok = false;
+	char line[256];
+
+	sprintf(line, "/flash/open %s\r\n", part);
+	if (!ser_iflush(fd) || !ser_write(fd, line)) {
+		goto out;
+	}
+
+	sprintf(line, "/flash/read 4 %zu %u\r\n", len, addr);
+	if (!ser_iflush(fd) || !ser_write(fd, line)) {
+		goto out;
+	}
+
+out:
+	sprintf(line, "/flash/close\r\n");
+	if (!ser_write(fd, line) || !ser_iflush(fd)) {
+		ok = false;
+	}
+
+	return ok;
+}
+
 bool cm_read(int fd, unsigned addr, void *buf, size_t len)
 {
 	if (len % 16) {
