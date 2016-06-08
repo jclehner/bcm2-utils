90183c4ef0ba481f8a3b459b420c3cde225cef06
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-06-08 19:01:39 +0200

    Add missing license info

--- cm.c
+++ cm.c
@@ -1,3 +1,22 @@
+/**
+ * bcm2-utils
+ * Copyright (C) 2016 Joseph C. Lehner <joseph.c.lehner@gmail.com>
+ *
+ * bcm2-utils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bcm2-utils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bcm2-utils.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "bcm2dump.h"
 
 static bool consume_lines(int fd)
