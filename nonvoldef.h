4c8edecac54c98449670e0053e9e36e81aeef4b5
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-09-14 12:05:24 +0200

    Add missing header

---
+++ nonvoldef.h
@@ -0,0 +1,25 @@
+#include "nonvol2.h"
+
+#define NV_VAR(type, name, ...) { name, make_shared<type>(__VA_ARGS__) }
+#define NV_VARN(type, name, ...) { name, nv_compound_rename(make_shared<type >(__VA_ARGS__), name) }
+#define NV_VAR2(type, name, ...) { name, sp<type>(new type(__VA_ARGS__)) }
+#define NV_VARN2(type, name, ...) { name, nv_compound_rename(sp<type>(new type(__VA_ARGS__)), name) }
+#define NV_VAR3(cond, type, name, ...) { name, nv_val_disable<type>(shared_ptr<type>(new type(__VA_ARGS__)), !(cond)) }
+#define NV_VARN3(cond, type, name, ...) { name, nv_compound_rename(nv_val_disable<type>(shared_ptr<type>(new type(__VA_ARGS__)), !(cond)), name) }
+
+#define COMMA() ,
+
+#define NV_GROUP(group, ...) make_shared<group>(__VA_ARGS__)
+#define NV_GROUP_DEF_CLONE(type) \
+		virtual type* clone() const override \
+		{ return new type(*this); }
+#define NV_GROUP_DEF_CTOR_AND_CLONE(type, magic, pretty) \
+		type() : nv_group(magic, pretty) {} \
+		\
+		NV_GROUP_DEF_CLONE(type)
+#define NV_COMPOUND_DEF_CTOR_AND_TYPE(ttype, tname) \
+		ttype(const string& name = "") : nv_compound(false) \
+		{ nv_compound::rename(name); } \
+		\
+		virtual string type() const override \
+		{ return tname; }
