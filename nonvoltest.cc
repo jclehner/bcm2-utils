00d6fc9dc37ba8397fde543ae0bdc964dce769df
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-09-16 21:36:47 +0200

    Remove unused stuff

--- nonvoltest.cc
+++ nonvoltest.cc
@@ -28,47 +28,7 @@ using namespace bcm2cfg;
 using namespace bcm2dump;
 using namespace std;
 
-void read_vars(istream& is, vector<nv_val::named>& vars)
-{
-	size_t pos = 0;
-
-	for (auto v : vars) {
-		if (!v.val->read(is)) {
-			cerr << "at pos " << pos << ": failed to parse " << v.val->type() << " (" << v.name << ")" << endl;
-			break;
-		}
-		pos += v.val->bytes();
-	}
-}
-
-void print_vars(const nv_val::list& vars)
-{
-	for (auto v : vars) {
-		if (v.val->is_set()) {
-			cerr << v.name << " = ";
-			cerr << v.val->to_pretty();
-		} else {
-			break;
-			cerr << "<not set>";
-		}
-		cerr << endl;
-	}
-}
-
-csp<nv_val> get(sp<nv_group> group, const string& name)
-{
-	vector<string> tok = split(name, '.', false, 2);
-	if (tok[0] == group->name()) {
-		if (tok.size() == 2) {
-			return group->find(tok[1]);
-		} else {
-			return group;
-		}
-	}
-
-	return nullptr;
-}
-
+namespace {
 int usage(bool help = false)
 {
 	ostream& os = logger::i();
@@ -273,10 +233,9 @@ int do_info(int argc, char** argv, const sp<settings>& settings)
 	return 0;
 }
 
-int main(int argc, char** argv)
+int do_main(int argc, char** argv)
 {
 	ios::sync_with_stdio();
-#if 1
 
 	int loglevel = logger::info;
 	string profile_name, password, key;
@@ -326,135 +285,34 @@ int main(int argc, char** argv)
 		return usage(false);
 	}
 
+	sp<profile> profile = !profile_name.empty() ? profile::get(profile_name) : nullptr;
+	sp<settings> settings = read_file(argv[1], format, profile, key, password);
+
+	if (cmd == "info") {
+		return do_info(argc, argv, settings);
+	} else if (!settings->is_valid()) {
+		throw user_error("invalid or encrypted file");
+	}
+
+	if (cmd == "encrypt" || cmd == "decrypt" || cmd == "fix") {
+		return do_crypt_or_fix(argc, argv, settings, key, password, pad);
+	} else if (cmd == "get" || cmd == "list") {
+		return do_list_or_get(argc, argv, settings);
+	} else if (cmd == "set") {
+		return do_set(argc, argv, settings);
+	}
+
+	return usage(false);
+}
+}
+
+int main(int argc, char** argv)
+{
 	try {
-		sp<profile> profile = !profile_name.empty() ? profile::get(profile_name) : nullptr;
-		sp<settings> settings = read_file(argv[1], format, profile, key, password);
-
-		if (cmd == "info") {
-			return do_info(argc, argv, settings);
-		} else if (!settings->is_valid()) {
-			throw user_error("invalid or encrypted file");
-		}
-
-		if (cmd == "encrypt" || cmd == "decrypt" || cmd == "fix") {
-			return do_crypt_or_fix(argc, argv, settings, key, password, pad);
-		} else if (cmd == "get" || cmd == "list") {
-			return do_list_or_get(argc, argv, settings);
-		} else if (cmd == "set") {
-			return do_set(argc, argv, settings);
-		}
-
-		usage(false);
+		return do_main(argc, argv);
 	} catch (const exception& e) {
 		logger::e() << "error: " << e.what() << endl;
 	}
 
 	return 1;
-
-
-#else
-
-	if (argc < 3) {
-		cerr << "usage: nonvoltest <type> <file> {get <name>, set <name> <value>}" << endl;
-		return 1;
-	}
-
-	logger::loglevel(logger::verbose);
-
-	ifstream in(argv[2]);
-	if (!in.good()) {
-		cerr << "failed to open " << argv[2] << endl;
-		return 1;
-	}
-
-	int type;
-
-	if (argv[1] == "group"s && false) {
-		type = nv_group::type_dyn;
-	} else if (argv[1] == "dyn"s) {
-		type = nv_group::type_dyn;
-	} else if (argv[1] == "gws"s) {
-		type = nv_group::type_dyn;
-	} else if (argv[1] == "perm"s) {
-		type = nv_group::type_perm;
-	} else {
-		cerr << "invalid type " << argv[1] << endl;
-		return 1;
-	}
-
-	sp<settings> cfg = settings::read(in, type, nullptr, "");
-	if (argc >= 5 && argv[3] == "get"s) {
-		csp<nv_val> val = cfg->get(argv[4]);
-		if (val) {
-			cout << argv[4] << " = " << val->to_pretty() << endl;
-		}
-	} else if (argc >= 6 && argv[3] == "set"s) {
-		cfg->set(argv[4], argv[5]);
-		cout << argv[4] << " = " << cfg->get(argv[4])->to_pretty() << endl;
-		in.close();
-		ofstream out(argv[2]);
-		cfg->write(out);
-	} else if (argc >= 4 && argv[3] == "info"s) {
-		cout << argv[2] << endl;
-		cout << cfg->header_to_string() << endl;
-		for (auto p : cfg->parts()) {
-			csp<nv_group> g = nv_val_cast<nv_group>(p.val);
-			string ugly = g->magic().to_str();
-			string pretty = g->magic().to_pretty();
-			cout << ugly << "  " << (ugly == pretty ? "    " : pretty) << "  ";
-			string version = g->version().to_pretty();
-			printf("%-6s  %-12s  %5zu b\n", version.c_str(), g->name().c_str(), g->bytes());
-		}
-		cout << endl;
-	} else if (argc >= 5 && (argv[3] == "list"s || argv[3] == "ls"s)) {
-
-		logger::loglevel(logger::info);
-
-		csp<nv_val> val = cfg->get(argv[4]);
-		if (val && val->is_compound()) {
-			for (auto p : nv_compound_cast(val)->parts()) {
-				ostream& os = starts_with(p.name, "_unk_") ? logger::v() : logger::i();
-				os << argv[4] << "." << p.name << (p.val->is_compound() ? ".*" : "") << endl;
-			}
-		}
-	} else {
-		cout << cfg->to_pretty() << endl;
-	}
-
-#if 0
-	while (in.good()) {
-		sp<nv_group> group;
-		if (nv_group::read(in, group, type) || in.eof()) {
-			if (!group || group->magic().to_str() == "ffffffff") {
-				break;
-			}
-
-			if (argc == 5 && argv[3] == "get"s) {
-				csp<nv_val> val = get(group, argv[4]);
-				if (val) {
-					cout << argv[4] << " = " << val->to_pretty() << endl;
-					break;
-				}
-			} else if (argc == 6 && argv[3] == "set"s) {
-				sp<nv_val> val = const_pointer_cast<nv_val>(get(group, argv[4]));
-				if (val) {
-					val->parse_checked(argv[5]);
-					cout << argv[4] << " = " << val->to_pretty() << endl;
-					ofstream out("grp_mod_" + group->name() + ".bin");
-					group->write(out);
-					break;
-				}
-			} else {
-				cout << group->magic() << " v" << group->version() << endl;
-				cout << group->to_pretty() << endl;
-				cout << endl;
-			}
-		} else {
-			return 1;
-		}
-	}
-#endif
-#endif
-
-	return 0;
 }
