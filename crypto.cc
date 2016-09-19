94fbc0cb2536af54af74679fcf6b1341d4f57003
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-09-19 12:29:30 +0200

    Fix OS X build

--- crypto.cc
+++ crypto.cc
@@ -1,13 +1,14 @@
 #include <stdexcept>
 #include <cstring>
 #include "crypto.h"
+#include "util.h"
 
 #if defined(__APPLE__)
 #include <CommonCrypto/CommonCrypto.h>
 #include <CommonCrypto/CommonDigest.h>
 
 #ifndef MD5_CTX
-#define MD5_CTX CC_MD5_CTX;
+#define MD5_CTX CC_MD5_CTX
 #define MD5_Init(ctx) CC_MD5_Init(ctx)
 #define MD5_Update(ctx, data, len) CC_MD5_Update(ctx, data, len)
 #define MD5_Final(md, ctx) CC_MD5_Final(md, ctx)
@@ -27,6 +28,7 @@
 #endif
 
 using namespace std;
+using namespace bcm2dump;
 
 namespace bcm2utils {
 namespace {
@@ -94,11 +96,12 @@ string crypt_aes_256_ecb(const string& ibuf, const string& key, bool encrypt)
 
 	return obuf;
 #elif defined(BCM2UTILS_USE_COMMON_CRYPTO)
-	size_t len = align_left(ibuf.size(), 16), moved;
+	size_t moved;
+	size_t len = align_left(ibuf.size(), 16);
 	string obuf(len, '\0');
 
 	CCCryptorStatus ret = CCCrypt(
-			encrypt ? kCCEncrypt ? kCCDecrypt,
+			encrypt ? kCCEncrypt : kCCDecrypt,
 			kCCAlgorithmAES128,
 			kCCOptionECBMode,
 			key.data(), kCCKeySizeAES256,
@@ -120,56 +123,4 @@ string crypt_aes_256_ecb(const string& ibuf, const string& key, bool encrypt)
 	return obuf;
 #endif
 }
-
-
-
-
-
-
-
-
-
 }
-
-
-#ifdef __APPLE__
-static int set_key(bool encrypt, const unsigned char *key, int bits, AES_KEY* akey)
-{
-	if (bits != 128 && bits != 256) {
-		return -1;
-	}
-
-	CCCryptorStatus ret = CCCryptorCreate(
-			encrypt ? kCCEncrypt : kCCDecrypt,
-			kCCAlgorithmAES128,
-			kCCOptionECBMode,
-			key,
-			bits == 256 ? kCCKeySizeAES256 : kCCKeySizeAES128,
-			NULL,
-			&akey->cryptor);
-
-	return ret == kCCSuccess ? 0 : -1;
-}
-
-static int crypt(const unsigned char* in, unsigned char* out, AES_KEY *akey)
-{
-	size_t moved;
-	CCCryptorStatus ret = CCCryptorUpdate(
-			akey->cryptor,
-			in, 16, out, 16,
-			&moved);
-	return ret == kCCSuccess ? 0 : -1;
-}
-
-inline int AES_set_encrypt_key(const unsigned char *key, const int bits, AES_KEY* akey)
-{ return set_key(true, key, bits, akey); }
-
-inline int AES_set_decrypt_key(const unsigned char *key, const int bits, AES_KEY* akey)
-{ return set_key(false, key, bits, akey); }
-
-int AES_encrypt(const unsigned char* in, unsigned char* out, AES_KEY* akey)
-{ return crypt(in, out, akey); }
-
-int AES_decrypt(const unsigned char* in, unsigned char* out, AES_KEY* akey)
-{ return crypt(in, out, akey); }
-#endif
