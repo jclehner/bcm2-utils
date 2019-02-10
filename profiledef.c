66acaa92f579f2a39965aa95354adbc36ea076b0
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2019-02-10 20:39:50 +0100

    Add missing comma

--- profiledef.c
+++ profiledef.c
@@ -406,7 +406,7 @@ struct bcm2_profile bcm2_profiles[] = {
 				.options = {
 					BCM2_VAL_STR("bfc:su_password", "ubeecable"),
 					BCM2_VAL_U32("bfc:conthread_priv_off", 0x74),
-					BCM2_VAL_U32("bfc:flash_reinit_on_retry", true)
+					BCM2_VAL_U32("bfc:flash_reinit_on_retry", true),
 					BCM2_VAL_U32("bfc:flash_read_direct", true)
 				},
 			},
