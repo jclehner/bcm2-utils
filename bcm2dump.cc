1c4b4f51b525e20580c1121c398c4e31edd841fc
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2019-12-29 16:21:41 +0100

    bcm2dump: allow multiple commands to be run in one go

--- bcm2dump.cc
+++ bcm2dump.cc
@@ -85,9 +85,9 @@ void usage(bool help = false)
 				"    start execution at a different address.\n\n";
 #endif
 	}
-	os << "  run   <interface> <command>" << endl;
+	os << "  run   <interface> <command 1> [<command 2> ...]" << endl;
 	if (help) {
-		os << "\n    Run command on the specified interface.\n\n";
+		os << "\n    Run command(s) on the specified interface.\n\n";
 	}
 	os << "  info  <interface>" << endl;
 	if (help) {
@@ -320,17 +320,20 @@ int do_write_exec(int argc, char** argv, int opts, const string& profile)
 
 int do_run(int argc, char** argv, const string& profile)
 {
-	if (argc != 3) {
+	if (argc < 3) {
 		usage(false);
 		return 1;
 	}
 
 	auto intf = interface::create(argv[1], profile);
-	intf->runcmd(argv[2]);
-	intf->foreach_line([] (const string& l) {
-			cout << trim(l) << endl;
-			return false;
-	}, 0, 5000);
+
+	for (int i = 2; i < argc; ++i) {
+		intf->runcmd(argv[i]);
+		intf->foreach_line([] (const string& l) {
+				cout << trim(l) << endl;
+				return false;
+		}, 0, 1000);
+	}
 
 	return 0;
 }
