946fe633048f6ddd10f63cc8b94c78544957c4f1
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2020-09-02 16:42:22 +0200

    Update fast3686 buffer

--- profiledef.c
+++ profiledef.c
@@ -407,7 +407,7 @@ struct bcm2_profile bcm2_profiles[] = {
 			{
 				.intf = BCM2_INTF_BFC,
 				.rwcode = 0x80002000,
-				.buffer = 0x85f00000,
+				.buffer = 0x88000000,
 				.options = {
 					BCM2_VAL_STR("bfc:su_password", "$agem001"),
 					BCM2_VAL_U32("bfc:conthread_priv_off", 0x74),
